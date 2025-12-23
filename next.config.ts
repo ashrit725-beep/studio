09b3e0a6da6b9f79ed2bb7be44a3569ad9caf99e
Bill tall <billtall61@gmail.com>
2025-12-23 18:45:02 +0000

    make the logos show up rn its just a ?mark and make the app go with the

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'aifire-6e5ac.appspot.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
