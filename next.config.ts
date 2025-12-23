3bef92e849501acc951939e8b6f73f3027ee5070
Bill tall <billtall61@gmail.com>
2025-12-23 19:57:33 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4mb',
+    },
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -29,16 +34,13 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https' ,
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
       },
     ],
   },
-  serverActions: {
-    bodySizeLimit: '4mb',
-  }
 };
 
 export default nextConfig;
